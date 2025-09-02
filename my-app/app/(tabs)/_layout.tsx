38841c52f9c56b7057f734c888feee8dde1f3915
Rebisco-Biscuit <23103808@usc.edu.ph>
2025-09-02 14:13:20 +0800

    spotify-login only

--- my-app/app/(tabs)/_layout.tsx
+++ my-app/app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+    <Tabs.Screen
+      name="Spotify"
+      options={{
+        title: 'Spotify',
+        tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+      }}
+    />
     </Tabs>
   );
 }
